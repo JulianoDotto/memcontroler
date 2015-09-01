e764c25f8c9e7c609d93555c1633a8dbd9bd6a4a
Juliano Dotto <juliano@juliano-Inspiron-5421>
2015-08-31 21:25:52 -0300

    Update -not finished yet

--- mem.sh
+++ mem.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+export BEEP=/usr/share/sounds/KDE-Im-Message-In.ogg
+alias beep='paplay $BEEP'
 
-#while :
-#do
+while :
+do
+	#varTop will execute top command, and will grep the 'KiB Mem' that contains the total memory and used memory
 	varTop=`top -bn1 | grep "KiB Mem"`
+	#With varTop we can 'isolate' the total and used memory in two var.
 	total=`echo $varTop | awk -F' '   '{print $3}'`
 	used=`echo $varTop | awk -F' '    '{print $5}'`
-	prct=0.6 |bc
-	warnprct=`echo $total * 0.6 | bc`
+	#Here we set a %
+	prct=0.6
+	#Here we take 60% of the total value
+	warnprct=$(echo "scale=2; $total * $prct" | bc)
+	warnprct=${warnprct/.*}
 
-	echo $warnprct
-
-#	if [$used -ge $prct]; then
-#		echo "opaaaa"
-#	else
-#		echo "ta de boa"
-#	fi		
-#done
+	if [ $used -ge $warnprct ];
+	then
+		beep
+	fi
+done
 
 
