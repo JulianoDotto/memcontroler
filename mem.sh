1d278075d4fa1ac1533d50ea0cee0c8cd50dcefd
Juliano Dotto <juliano.dotto@hotmail.com>
2015-09-02 10:15:01 -0300

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -9,12 +9,14 @@ do
 	used=`echo $varTop | awk -F' '    '{print $5}'`
 	#Here we set a %
 	prct=0.9
-	#Here we take 60% of the total value
+	#Here we take 90% of the total value
 	warnprct=$(echo "scale=2; $total * $prct" | bc)
+	#transform the warnprct in int
 	warnprct=${warnprct/.*}
-
+	#Compare if memory used it`s above or equal warnprct
 	if [ $used -ge $warnprct ];
 	then
+		#notify the user that the memory is above 90%
 		notify-send "Memory used it's above 90%"
 		sleep 30
 	fi
