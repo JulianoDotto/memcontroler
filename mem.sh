11651c66619acab31467707442ad21c83a03ca84
Juliano Dotto <juliano@juliano-Inspiron-5421>
2015-09-04 21:13:13 -0300

    User can set the %

--- mem.sh
+++ mem.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read prct
+
 while :
 do
 	#varTop will execute top command, and will grep the 'KiB Mem' that contains the total memory and used memory
@@ -8,16 +10,14 @@ do
 	total=`echo $varTop | awk -F' '   '{print $3}'`
 	used=`echo $varTop | awk -F' '    '{print $5}'`
 	#Here we set a %
-	prct=0.9
+	#prct=0.9
 	#Here we take 60% of the total value
 	warnprct=$(echo "scale=2; $total * $prct" | bc)
 	warnprct=${warnprct/.*}
 
 	if [ $used -ge $warnprct ];
 	then
-		notify-send "Memory used it's above 90%"
+		notify-send "NOTIFY: Memory used it's too high"
 		sleep 30
 	fi
 done
-
-
