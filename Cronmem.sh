246fbe3ab00cf1913d1adacfcf76ec801ca36f19
Juliano Dotto <juliano@juliano-Inspiron-5421>
2015-09-22 20:50:49 -0300

    Last Version

--- Cronmem.sh
+++ Cronmem.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-while :
-do
-
 	#varTop will execute top command, and will grep the 'KiB Mem' that contains the total memory and used memory
 	varTop=`top -bn1 | grep "KiB Mem"`
 	#With varTop we can 'isolate' the total and used memory in two var.
 	total=`echo $varTop | awk -F' '   '{print $3}'`
 	used=`echo $varTop | awk -F' '    '{print $5}'`
 	#Here we set a % when we call the script (ex: ./mem.sh 0.5)
-	prct=$1
+	prct=0.9
 	#Here we take % of the total value
 	warnprct=$(echo "scale=2; $total * $prct" | bc)
 	warnprct=${warnprct/.*}
@@ -17,6 +14,4 @@ do
 	if [ $used -ge $warnprct ];
 	then
 		notify-send "NOTIFY: Memory used it's too high"
-		sleep 30;
 	fi
-done
